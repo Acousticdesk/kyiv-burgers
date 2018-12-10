7b2ce4351597b35d084b5a3c2e73a440d41fe62b
Andrii Kicha <akicha@cogniance.com>
2018-12-10 17:06:03 +0200

    Fix for Jekyll plugin

---
+++ precache-manifest.88704c95f6918ad49d890d7ca0b08c9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6547c0a3eb5124097cee",
+    "url": "/kyiv-burgers/static/js/runtime~main.6547c0a3.js"
+  },
+  {
+    "revision": "1da9d1bec18118c27ed0",
+    "url": "/kyiv-burgers/static/js/main.1da9d1be.chunk.js"
+  },
+  {
+    "revision": "bf3442e56e76a643ea3f",
+    "url": "/kyiv-burgers/static/js/1.bf3442e5.chunk.js"
+  },
+  {
+    "revision": "1da9d1bec18118c27ed0",
+    "url": "/kyiv-burgers/static/css/main.a395be7b.chunk.css"
+  },
+  {
+    "revision": "70b3b147da8e9eac050dbbdc1b41af12",
+    "url": "/kyiv-burgers/index.html"
+  }
+];
